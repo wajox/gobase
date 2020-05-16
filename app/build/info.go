99b605c5377e529b481b7fb09e7f5681b4b9ba20
Ildar Usmanov <wajox@mail.ru>
2020-05-17 00:08:52 +0300

    update gitignore

---
+++ app/build/info.go
@@ -0,0 +1,29 @@
+package build
+
+import (
+	"runtime"
+)
+
+// Info represents all available build & runtime environment information.
+type Info struct {
+	Version    string `json:"version,omitempty"`
+	CommitHash string `json:"commit_hash,omitempty"`
+	BuildDate  string `json:"build_date,omitempty"`
+	GoVersion  string `json:"go_version,omitempty"`
+	Os         string `json:"os,omitempty"`
+	Arch       string `json:"arch,omitempty"`
+	Compiler   string `json:"compiler,omitempty"`
+}
+
+// NewApplicationInfo returns all available build information.
+func NewInfo() *Info {
+	return &Info{
+		Version:    Version,
+		CommitHash: CommitHash,
+		BuildDate:  BuildDate,
+		GoVersion:  runtime.Version(),
+		Os:         runtime.GOOS,
+		Arch:       runtime.GOARCH,
+		Compiler:   runtime.Compiler,
+	}
+}
---
+++ app/build/version.go
@@ -0,0 +1,9 @@
+package build
+
+// Provisioned by ldflags. DO NOT TOUCH IT
+// nolint: gochecknoglobals
+var (
+	Version    string
+	CommitHash string
+	BuildDate  string
+)
