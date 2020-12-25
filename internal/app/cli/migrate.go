e4c1d03087fde5e7cda51410902e4f2dea4bd603
Ildar Usmanov <wajox@mail.ru>
2020-12-25 16:16:51 +0300

    update comments

--- internal/app/cli/migrate.go
+++ internal/app/cli/migrate.go
@@ -1,14 +1,6 @@
-//nolint //reason: TODO. disabled just due to developing process. uncomment it when module will be done
 package cli
 
 import (
-	// "os"
-	// "os/signal"
-	// "syscall"
-	// "time"
-
-	// "github.com/wajox/gobase/internal/app"
-	// "github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
@@ -22,24 +14,7 @@ func NewMigrateCmd() *cobra.Command {
 		Aliases: []string{"migrate"},
 		Short:   "run migrations for database",
 		Run: func(cmd *cobra.Command, args []string) {
-			// log.Info().Msg("Starting")
-
-			// application, err := app.InitializeApplication()
-
-			// if err != nil {
-			// 	log.Error().Err(err).Msg("can not initialize application")
-			// } else {
-			// 	application.Start(true)
-
-			// 	log.Info().Msg("Started")
-			// }
-
-			// run migrations
-
-			// log.Error().Err(application.Stop()).Msg("stop application")
-
-			// time.Sleep(time.Second)
-			// log.Info().Msg("Finished")
+			// implement here you migration command
 		},
 	}
 }
--- internal/app/cli/seed.go
+++ internal/app/cli/seed.go
@@ -1,14 +1,6 @@
-//nolint //reason: TODO. disabled just due to developing process. uncomment it when module will be done
 package cli
 
 import (
-	// "os"
-	// "os/signal"
-	// "syscall"
-	// "time"
-
-	// "github.com/wajox/gobase/internal/app"
-	// "github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
@@ -18,24 +10,7 @@ func NewSeedCmd() *cobra.Command {
 		Aliases: []string{"seed"},
 		Short:   "apply seeds for database",
 		Run: func(cmd *cobra.Command, args []string) {
-			// log.Info().Msg("Starting")
-
-			// application, err := app.InitializeApplication()
-
-			// if err != nil {
-			// 	log.Error().Err(err).Msg("can not initialize application")
-			// } else {
-			// 	application.Start(true)
-
-			// 	log.Info().Msg("Started")
-			// }
-
-			// run seeds
-
-			// log.Error().Err(application.Stop()).Msg("stop application")
-
-			// time.Sleep(time.Second)
-			// log.Info().Msg("Finished")
+			// implement your seed command for DB setup
 		},
 	}
 }
