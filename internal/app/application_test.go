3dc7435da05b0d9e0952763c7557bf534994067a
Ildar Usmanov <wajox@mail.ru>
2021-01-15 16:16:03 +0300

    update pkgs, update tests

--- internal/app/application_test.go
+++ internal/app/application_test.go
@@ -1,6 +1,8 @@
 package app_test
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -14,6 +16,29 @@ var _ = Describe("Application", func() {
 
 			Expect(app).NotTo(BeNil())
 			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("methods", func() {
+		var (
+			app *Application
+		)
+
+		BeforeEach(func() {
+			app, _ = InitializeApplication()
+		})
+
+		Describe("Start(), Stop()", func() {
+			It("should start and stop application", func() {
+				ctx, cancel := context.WithCancel(context.Background())
+				app.Start(ctx, false)
+
+				defer cancel()
+
+				err := app.Stop()
+
+				Expect(err).To(BeNil())
+			})
 		})
 	})
 })
